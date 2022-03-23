7cbecd84340cbaad3dd41305cf169df53d4509a4
Zuzana <zuza0449@edu.easj.dk>
2022-03-23 20:42:44 +0100

    Added new page idk

---
+++ main.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".pin-button-braetspil")
+let addHere = document.querySelector(".selected-items-table")
+btn.addEventListener("click", function() {
+    addHere.innerHTML = "Hi"
+    
+})
